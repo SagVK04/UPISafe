e811b0102017dda2f7fe48a27deca20d9b53fa98
Sagnik Metiya <metiyasagnik692@gmail.com>
2026-03-11 02:12:14 +0530

    Input Fields Modified

--- app/src/main/java/com/example/upisafe/TransactionAdapter.kt
+++ app/src/main/java/com/example/upisafe/TransactionAdapter.kt
@@ -13,7 +13,8 @@ class TransactionAdapter(private val transactionList: List<TransactionModel>) :
         val tvAmount: TextView = itemView.findViewById(R.id.tvAmount)
         val tvDate: TextView = itemView.findViewById(R.id.tvDate)
         val tvTime: TextView = itemView.findViewById(R.id.tvTime)
-        val tvPlatform: TextView = itemView.findViewById(R.id.tvPlatform)
+        val upi_id: TextView = itemView.findViewById(R.id.rec_id)
+        val trans_type: TextView = itemView.findViewById(R.id.tr_type)
         val  tvresult: TextView = itemView.findViewById(R.id.tvResult1)
 
         val res_score: TextView = itemView.findViewById(R.id.tvResult_score)
@@ -30,13 +31,15 @@ class TransactionAdapter(private val transactionList: List<TransactionModel>) :
         holder.tvAmount.text = "Amount: ₹${transaction.amount}"
         holder.tvDate.text = "Date: ${transaction.date}"
         holder.tvTime.text = "Time: ${transaction.time}"
-        holder.tvPlatform.text = "Platform: ${transaction.platform}"
+        holder.upi_id.text = "Receiver's UPI Id:: ${transaction.id}"
+        holder.trans_type.text = "Transaction Type: ${transaction.tr_type}"
         holder.tvresult.text = "Result: ${transaction.result}"
         holder.res_score.text = "Risk Score: ${transaction.riskScore}"
         holder.tvAmount.textSize = 20F
         holder.tvDate.textSize = 20F
         holder.tvTime.textSize = 20F
-        holder.tvPlatform.textSize = 20F
+        holder.upi_id.textSize = 20F
+        holder.trans_type.textSize = 20F
         holder.tvresult.textSize = 20F
         holder.res_score.textSize = 20F
     }
--- app/src/main/java/com/example/upisafe/TransactionModel.kt
+++ app/src/main/java/com/example/upisafe/TransactionModel.kt
@@ -3,9 +3,10 @@ package com.example.upisafe
 data class TransactionModel(
     val amount: String = "",
     val date: String = "",
-    val platform: String = "",
+    val time: String = "",
+    val id: String = "",
+    val tr_type: String = "",
     val result: String = "", // "Safe" or "Fraud"
     val riskScore: String = "", // "85%"
-    val time: String = "",
     val timestamp: Long = System.currentTimeMillis()
 )
